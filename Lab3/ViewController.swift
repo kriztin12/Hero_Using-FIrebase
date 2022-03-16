9c32f41c387f06b846c7498d61b59f95bde38f8d
Kriztin <kabe0004@student.monash.edu>
2022-03-16 11:26:30 +1100

    Initial Commit

---
+++ Lab3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab3
+//
+//  Created by Kriztin Abellon on 16/3/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
